63cdfff42b57edc277e05a7b0dced0f0cdffcef9
Quentin Kniep <hello@quentinkniep.com>
2025-04-07 14:24:32 +0200

    do not redownload ping data if it already exists

--- download_data.sh
+++ download_data.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 cd data
-wget https://wp-public.s3.amazonaws.com/pings/pings-2020-07-19-2020-07-20.csv.gz
-gzip -d pings-2020-07-19-2020-07-20.csv.gz
+if [ ! -f ./pings-2020-07-19-2020-07-20.csv ]; then
+	echo "Downloading ping dataset..."
+	wget https://wp-public.s3.amazonaws.com/pings/pings-2020-07-19-2020-07-20.csv.gz
+	gzip -d pings-2020-07-19-2020-07-20.csv.gz
+else
+    echo "data/pings-2020-07-19-2020-07-20.csv already exists"
+fi
