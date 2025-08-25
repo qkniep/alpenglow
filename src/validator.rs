2a8b9bafa8f27c5b427c8990653d9a248aed24fd
Quentin Kniep <hello@quentinkniep.com>
2025-08-25 19:26:26 +0200

    extend `Validator`

--- src/validator.rs
+++ src/validator.rs
@@ -1,4 +1,25 @@
 // Copyright (c) Anza Technology, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-pub struct Validator {}
+use crate::execution::Execution;
+use crate::network::Network;
+use crate::{All2All, Alpenglow, Disseminator};
+
+/// Full validator node.
+///
+/// Consist of a consensus protocol instance and an execution engine.
+/// Participates in consensus and executes finaliztransactions.
+pub struct Validator<A, D, R, T, E>
+where
+    A: All2All,
+    D: Disseminator,
+    R: Network,
+    T: Network,
+    E: Execution,
+{
+    /// Consensus protocol instance.
+    consensus: Alpenglow<A, D, R, T>,
+
+    /// Execution engine.
+    execution: E,
+}
