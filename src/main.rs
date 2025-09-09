01eb3cee1324151c8392d5cb21fc26973bcfeba9
Quentin Kniep <hello@quentinkniep.com>
2025-09-09 15:03:50 +0200

    undo change

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ async fn main() -> Result<()> {
     // enable fancy `color_eyre` error messages
     color_eyre::install()?;
 
+    // enable `fastrace` tracing
     let reporter = OpenTelemetryReporter::new(
         SpanExporter::builder()
             .with_tonic()
