ba727c5cb40b0145846abd4e547d3ce7469185f9
akhi singhania <akhi3030@gmail.com>
2025-09-25 06:19:49 +0000

    clippy

--- benches/shredder.rs
+++ benches/shredder.rs
@@ -43,8 +43,8 @@ fn deshred<S: Shredder>(bencher: divan::Bencher) {
             let sk = SecretKey::new(&mut rng);
             let mut shreds = S::shred(slice, &sk).unwrap().map(Some);
             // need at least DATA_SHREDS to reconstruct and want to include as many coding shreds as possible which should be at the end of the array
-            for i in 0..TOTAL_SHREDS - DATA_SHREDS {
-                shreds[i] = None;
+            for shred in shreds.iter_mut().take(TOTAL_SHREDS - DATA_SHREDS) {
+                *shred = None;
             }
             shreds
         })
