0db6606349467cf89c40eb939d2e67001a8a85e9
Quentin Kniep <hello@quentinkniep.com>
2025-10-01 21:09:00 +0200

    nit: derive order more consistent

--- src/consensus/vote.rs
+++ src/consensus/vote.rs
@@ -18,7 +18,7 @@ use crate::{Slot, ValidatorId};
 /// allowing type-specific data to be authenticated and verified.
 ///
 /// This struct is produced by signing the bytes of a `VoteKind` instance.
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Vote {
     kind: VoteKind,
     sig: IndividualSignature,
@@ -26,7 +26,7 @@ pub struct Vote {
 }
 
 /// Represents the type-specific vote payload as per the protocol.
-#[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub enum VoteKind {
     /// A notarization vote for a given block hash in a given slot.
     Notar(Slot, Hash),
