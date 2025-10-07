be4e59a982958c68731da46e860f9d00a2ef72c6
Quentin Kniep <hello@quentinkniep.com>
2025-10-07 20:30:38 +0200

    helper methodds in `TurbineInstance`

--- src/bin/simulations/turbine.rs
+++ src/bin/simulations/turbine.rs
@@ -7,9 +7,9 @@
 //! - Latency simulation for block dissemination via Turbine.
 //! - Robustness simulation against liveness and safety failures.
 
-use alpenglow::ValidatorId;
 use alpenglow::disseminator::rotor::SamplingStrategy;
-use alpenglow::disseminator::turbine::{DEFAULT_FANOUT, TurbineTree};
+use alpenglow::disseminator::turbine::{DEFAULT_FANOUT, WeightedShuffle};
+use alpenglow::{ValidatorId, ValidatorInfo};
 use rand::prelude::*;
 
 use crate::discrete_event_simulator::Builder;
@@ -46,14 +46,16 @@ impl TurbineParams {
 #[derive(Debug)]
 pub struct TurbineInstanceBuilder<L: SamplingStrategy> {
     pub leader_sampler: L,
+    pub validators: Vec<ValidatorInfo>,
     pub params: TurbineParams,
 }
 
 impl<L: SamplingStrategy> TurbineInstanceBuilder<L> {
     /// Creates a new builder instance, with the provided sampling strategies.
-    pub fn new(leader_sampler: L, params: TurbineParams) -> Self {
+    pub fn new(leader_sampler: L, validators: Vec<ValidatorInfo>, params: TurbineParams) -> Self {
         Self {
             leader_sampler,
+            validators,
             params,
         }
     }
@@ -64,9 +66,24 @@ impl<L: SamplingStrategy> Builder for TurbineInstanceBuilder<L> {
     type Instance = TurbineInstance;
 
     fn build(&self, rng: &mut impl Rng) -> TurbineInstance {
+        let mut build_tree = || {
+            let mut weighted_shuffle =
+                WeightedShuffle::new(self.validators.iter().map(|v| v.stake));
+            weighted_shuffle
+                .shuffle(rng)
+                .map(|i| i as ValidatorId)
+                .collect::<Vec<_>>()
+        };
+        let trees: Vec<_> = (0..self.params.num_slices)
+            .map(|_| {
+                (0..self.params.num_shreds)
+                    .map(|_| build_tree())
+                    .collect::<Vec<_>>()
+            })
+            .collect();
         TurbineInstance {
             leader: self.leader_sampler.sample(rng),
-            tree: TurbineTree::new(),
+            trees,
             params: self.params,
         }
     }
@@ -81,8 +98,48 @@ impl<L: SamplingStrategy> Builder for TurbineInstanceBuilder<L> {
 pub struct TurbineInstance {
     /// Leader validator.
     pub leader: ValidatorId,
-    /// Relays for each slice, and each shred within a slice.
-    pub tree: TurbineTree,
+    /// Turbine tree for each slice.
+    pub trees: Vec<Vec<Vec<ValidatorId>>>,
     /// Parameters this instance corresponds to.
     pub params: TurbineParams,
+}
+
+impl TurbineInstance {
+    pub fn parent(&self, slice: usize, shred: usize, v: ValidatorId) -> Option<ValidatorId> {
+        if v == 0 {
+            None
+        } else {
+            self.trees[slice][shred]
+                .get(v as usize / self.params.fanout)
+                .copied()
+        }
+    }
+
+    pub fn layer1_parents(&self, slice: usize, v: ValidatorId) -> Vec<ValidatorId> {
+        (0..self.params.num_shreds)
+            .filter(|shred| self.layer1(slice, *shred).contains(&v))
+            .filter_map(|shred| self.parent(slice, shred, v))
+            .collect()
+    }
+
+    pub fn layer2_parents(&self, slice: usize, v: ValidatorId) -> Vec<ValidatorId> {
+        (0..self.params.num_shreds)
+            .filter(|shred| {
+                !self.layer1(slice, *shred).contains(&v) && self.root(slice, *shred) != v
+            })
+            .filter_map(|shred| self.parent(slice, shred, v))
+            .collect()
+    }
+
+    pub fn root(&self, slice: usize, shred: usize) -> ValidatorId {
+        self.trees[slice][shred][0]
+    }
+
+    pub fn layer1(&self, slice: usize, shred: usize) -> &[ValidatorId] {
+        &self.trees[slice][shred][1..self.params.fanout + 1]
+    }
+
+    pub fn layer2(&self, slice: usize, shred: usize) -> &[ValidatorId] {
+        &self.trees[slice][shred][self.params.fanout + 1..]
+    }
 }
