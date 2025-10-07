55a5de7d3241af628e1ab2bc77847856d33a9ce0
Quentin Kniep <hello@quentinkniep.com>
2025-10-07 19:10:54 +0200

    docs + clippy

--- src/bin/simulations/ryse.rs
+++ src/bin/simulations/ryse.rs
@@ -6,7 +6,9 @@
 //! Ryse is one instantiation of a multiple-concurrent proposers (MCP) consensus protocol.
 //! As such, it provides the following economic properties:
 //! - Selective-censorship resistance:
-//! - Hiding:
+//!   A malicious consensus leader can not unilaterally exclude an honest proposer's proposal.
+//! - Weak hiding:
+//!   A malicious proposer cannot condition the contents of their block on the contents of another.
 //!
 //! Compared to Pyjama, Ryse aims to be a simple modification of Alpenglow.
 //! That is, it is not a general gadget that can be combined with any consensus protocol.
@@ -17,9 +19,6 @@ mod latency;
 mod parameters;
 mod robustness;
 
-pub use self::latency::{
-    LatencyEvent, LatencySimInstance, LatencySimInstanceBuilder, LatencySimParams,
-    RyseLatencySimulation,
-};
-pub use self::parameters::{RyseInstance, RyseInstanceBuilder, RyseParameters};
+pub use self::latency::{LatencySimInstanceBuilder, LatencySimParams, RyseLatencySimulation};
+pub use self::parameters::{RyseInstanceBuilder, RyseParameters};
 pub use self::robustness::run_robustness_tests;
