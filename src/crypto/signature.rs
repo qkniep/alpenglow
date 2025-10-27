99b0ecca36d4e56dea8d5d19dc3b5b674a010bee
Quentin Kniep <hello@quentinkniep.com>
2025-10-27 19:44:20 +0100

    `rustfmt`

--- src/crypto/signature.rs
+++ src/crypto/signature.rs
@@ -11,7 +11,8 @@ use ed25519_consensus::{SigningKey, VerificationKey};
 use rand::CryptoRng;
 use serde::{Deserialize, Serialize};
 use static_assertions::const_assert_eq;
-use wincode::{SchemaRead, SchemaWrite, containers::Pod};
+use wincode::containers::Pod;
+use wincode::{SchemaRead, SchemaWrite};
 
 /// Size of an ed25519 signature.
 const SIGNATURE_SIZE: usize = 64;
