8c10847ac939098c3329b5913a55b1f98b1efb35
Quentin Kniep <hello@quentinkniep.com>
2025-11-05 23:39:58 +0100

    actually run smoke tests in CI

--- test.sh
+++ test.sh
@@ -43,11 +43,13 @@ smoke_tests () {
 if [ $# -gt 0 ] && [ $1 == "slow" ]; then
 	slow_tests
 elif [ $# -gt 0 ] && [ $1 == "ci" ]; then
-	fast_tests && doc_tests && sequential_tests
+	fast_tests && doc_tests && sequential_tests && smoke_tests
 elif [ $# -gt 0 ] && [ $1 == "doc" ]; then
 	doc_tests
 elif [ $# -gt 0 ] && [ $1 == "sequential" ]; then
 	sequential_tests
+elif [ $# -gt 0 ] && [ $1 == "smoke" ]; then
+	smoke_tests
 elif [ $# -gt 0 ] && [ $1 == "many" ]; then
 	echo "🔁 Running tests for 50 iterations to detect flaky tests..."
 	for i in $(seq 1 50); do
