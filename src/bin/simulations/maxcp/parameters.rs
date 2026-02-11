c863ff170e11859bf4c54780a05f4768b782aee0
Quentin Kniep <hello@quentinkniep.com>
2026-02-11 09:43:30 +0100

    adapt attacks and params

--- src/bin/simulations/maxcp/parameters.rs
+++ src/bin/simulations/maxcp/parameters.rs
@@ -5,6 +5,8 @@
 //!
 //!
 
+use std::time::Duration;
+
 use alpenglow::ValidatorId;
 use alpenglow::disseminator::rotor::SamplingStrategy;
 use log::info;
@@ -17,11 +19,16 @@ use crate::discrete_event_simulator::Builder;
 #[derive(Clone, Copy, Debug)]
 pub struct MaxcpParameters {
     pub num_proposers: u64,
+    pub num_attestors: u64,
     pub num_relays: u64,
-    pub can_decode_threshold: u64,
+    pub can_decode_proposal_threshold: u64,
+    pub can_decode_block_threshold: u64,
     pub should_decode_threshold: u64,
     pub attestations_threshold: u64,
-    pub num_slices: u64,
+    pub slot_time: Duration,
+    pub num_batches: u64,
+    pub slices_per_batch: u64,
+    pub quick_release: bool,
 }
 
 /// Specific instance of the MaxCP protocol.
@@ -98,96 +105,46 @@ pub struct AdversaryStrength {
 
 impl MaxcpParameters {
     /// Generates a new balanced parameter set, equally resistant against all attacks.
-    pub fn new(num_proposers: u64, num_relays: u64) -> Self {
+    pub fn new(num_proposers: u64, num_attestors: u64, num_relays: u64) -> Self {
         Self {
             num_proposers,
+            num_attestors,
             num_relays,
-            can_decode_threshold: (num_relays * 25).div_ceil(100),
-            should_decode_threshold: (num_relays * 50).div_ceil(100),
-            attestations_threshold: (num_relays * 75).div_ceil(100),
-            num_slices: 1,
+            can_decode_proposal_threshold: (num_attestors * 25).div_ceil(100),
+            can_decode_block_threshold: (num_relays * 50).div_ceil(100),
+            should_decode_threshold: (num_attestors * 25).div_ceil(100),
+            attestations_threshold: (num_attestors * 50).div_ceil(100),
+            slot_time: Duration::from_millis(400),
+            num_batches: 20,
+            slices_per_batch: 1,
+            quick_release: false,
         }
     }
 
-    /// Generates a new parameter set based on the first ones proposed in the PJM paper.
-    pub fn new_paper1(num_proposers: u64, num_relays: u64) -> Self {
+    /// Generates a new parameter set where attestors release shreds themselves.
+    pub fn new_quick_release(num_proposers: u64, num_attestors: u64, num_relays: u64) -> Self {
         Self {
             num_proposers,
+            num_attestors,
             num_relays,
-            can_decode_threshold: (num_relays * 40).div_ceil(100),
-            should_decode_threshold: (num_relays * 60).div_ceil(100),
-            attestations_threshold: (num_relays * 80).div_ceil(100),
-            num_slices: 1,
-        }
-    }
-
-    /// Generates a new parameter set based on the second ones proposed in the PJM paper.
-    pub fn new_paper2(num_proposers: u64, num_relays: u64) -> Self {
-        Self {
-            num_proposers,
-            num_relays,
-            can_decode_threshold: (num_relays * 30).div_ceil(100),
-            should_decode_threshold: (num_relays * 60).div_ceil(100),
-            attestations_threshold: (num_relays * 80).div_ceil(100),
-            num_slices: 1,
-        }
-    }
-
-    /// Generates a new parameter set based on the second ones proposed in the Supernova paper.
-    pub fn new_supernova(num_proposers: u64, num_relays: u64) -> Self {
-        Self {
-            num_proposers,
-            num_relays,
-            can_decode_threshold: (num_relays * 20).div_ceil(100),
-            should_decode_threshold: (num_relays * 40).div_ceil(100),
-            attestations_threshold: (num_relays * 60).div_ceil(100),
-            num_slices: 1,
-        }
-    }
-
-    /// Generates a new parameter set prioritizing hiding over liveness.
-    pub fn new_hiding(num_proposers: u64, num_relays: u64) -> Self {
-        Self {
-            num_proposers,
-            num_relays,
-            can_decode_threshold: (num_relays * 40).div_ceil(100),
-            should_decode_threshold: (num_relays * 60).div_ceil(100),
-            attestations_threshold: (num_relays * 80).div_ceil(100),
-            num_slices: 1,
-        }
-    }
-
-    /// Generates a new parameter set prioritizing liveness over hiding.
-    pub fn new_liveness(num_proposers: u64, num_relays: u64) -> Self {
-        Self {
-            num_proposers,
-            num_relays,
-            can_decode_threshold: (num_relays * 20).div_ceil(100),
-            should_decode_threshold: (num_relays * 47).div_ceil(100),
-            attestations_threshold: (num_relays * 73).div_ceil(100),
-            num_slices: 1,
-        }
-    }
-
-    /// Generates a new parameter set prioritizing permanent liveness failures over all others.
-    pub fn new_permanent_liveness(num_proposers: u64, num_relays: u64) -> Self {
-        Self {
-            num_proposers,
-            num_relays,
-            can_decode_threshold: (num_relays * 23).div_ceil(100),
-            should_decode_threshold: (num_relays * 53).div_ceil(100),
-            attestations_threshold: (num_relays * 76).div_ceil(100),
-            num_slices: 1,
+            can_decode_proposal_threshold: (num_attestors * 40).div_ceil(100),
+            can_decode_block_threshold: (num_relays * 50).div_ceil(100),
+            should_decode_threshold: (num_attestors * 60).div_ceil(100),
+            attestations_threshold: (num_attestors * 80).div_ceil(100),
+            slot_time: Duration::from_millis(400),
+            num_batches: 20,
+            slices_per_batch: 1,
+            quick_release: true,
         }
     }
 
     /// Proobability that the adversary can break the hiding property in a slot.
     pub fn break_hiding_probability(&self, adv_strength: AdversaryStrength) -> f64 {
-        // probability that the adversary controls enough relays to decrypt
+        // probability that the adversary controls enough attestors to decrypt
         let byzantine = adv_strength.byzantine;
-        let relays_dist = Binomial::new(byzantine, self.num_relays).unwrap();
-        let relays_needed = self.can_decode_threshold;
-        1.0 - relays_dist.cdf(relays_needed - 1)
+        let attestors_dist = Binomial::new(byzantine, self.num_attestors).unwrap();
+        let attestors_needed = self.can_decode_proposal_threshold;
+        1.0 - attestors_dist.cdf(attestors_needed - 1)
     }
 
     /// Probability that the adversary can selectively censor proposers in a slot.
@@ -201,12 +158,12 @@ impl MaxcpParameters {
 
         // probability that the adversary can exclude all proposers
         let byzantine = adv_strength.byzantine;
-        let relays_dist = Binomial::new(byzantine, self.num_relays).unwrap();
-        let relays_needed = self.attestations_threshold - self.should_decode_threshold;
-        let prob_censor_relays = 1.0 - relays_dist.cdf(relays_needed - 1);
+        let attestors_dist = Binomial::new(byzantine, self.num_attestors).unwrap();
+        let attestors_needed = self.attestations_threshold - self.should_decode_threshold;
+        let prob_censor_attestors = 1.0 - attestors_dist.cdf(attestors_needed - 1);
 
         // probability that either attack works
-        1.0 - (1.0 - prob_all_proposers) * (1.0 - prob_censor_relays)
+        1.0 - (1.0 - prob_all_proposers) * (1.0 - prob_censor_attestors)
     }
 
     /// Probability that the adversary can cause a temporary liveness failure in a slot.
@@ -217,29 +174,24 @@ impl MaxcpParameters {
         let prob_no_proposals = 1.0 - proposers_dist.cdf(self.num_proposers - 1);
 
         // probability that the adversary can prevent the leader from producing a non-empty block
+        let attestors_dist = Binomial::new(failed, self.num_attestors).unwrap();
+        let attestors_to_hold_protocol =
+            self.should_decode_threshold - self.can_decode_proposal_threshold;
+        let attestors_to_censor_proposers =
+            self.attestations_threshold - self.should_decode_threshold;
+        let attestors_to_censor_leader = self.num_attestors - self.attestations_threshold;
+        let attestors_needed = attestors_to_hold_protocol
+            .min(attestors_to_censor_proposers)
+            .min(attestors_to_censor_leader);
+        let prob_censor_attestors = 1.0 - attestors_dist.cdf(attestors_needed - 1);
+
+        // probability that the adversary can prevent Rotor from forwarding block
         let relays_dist = Binomial::new(failed, self.num_relays).unwrap();
-        let relays_to_hold_protocol = self.should_decode_threshold - self.can_decode_threshold;
-        let relays_to_censor_proposers = self.attestations_threshold - self.should_decode_threshold;
-        let relays_to_censor_leader = self.num_relays - self.attestations_threshold;
-        let relays_needed = relays_to_hold_protocol
-            .min(relays_to_censor_proposers)
-            .min(relays_to_censor_leader);
+        let relays_needed = self.num_relays - self.can_decode_block_threshold;
         let prob_censor_relays = 1.0 - relays_dist.cdf(relays_needed - 1);
 
-        // probability that either attack works
-        1.0 - (1.0 - prob_no_proposals) * (1.0 - prob_censor_relays)
-    }
-
-    /// Probability that the adversary can cause a permanent liveness failure.
-    ///
-    /// The adversary can achieve this by withholding enough shreds that should be revealed.
-    /// This analyzes the worst case where a batch got `self.should_decode_threshold` attestations.
-    pub fn permanent_liveness_failure_probability(&self, adv_stength: AdversaryStrength) -> f64 {
-        // probability that the adversary can withhold enough shreds
-        let byzantine = adv_stength.byzantine;
-        let relays_dist = Binomial::new(byzantine, self.num_relays).unwrap();
-        let relays_needed = self.should_decode_threshold - self.can_decode_threshold;
-        1.0 - relays_dist.cdf(relays_needed - 1)
+        // probability that any attack works
+        1.0 - (1.0 - prob_no_proposals) * (1.0 - prob_censor_attestors) * (1.0 - prob_censor_relays)
     }
 
     /// Calculates and prints attack sucess probabilities.
@@ -247,10 +199,11 @@ impl MaxcpParameters {
     /// Capabilities of the adversary are specified in the `adv_strength` parameter.
     pub fn print_failure_probabilities(&self, adv_strength: AdversaryStrength) {
         info!(
-            "MaxCP parameters: proposers={}, relays={}, {:.2}/{:.2}/{:.2}",
+            "MaxCP parameters: proposers={}, relays={}, {:.2}/{:.2}/{:.2}/{:.2}",
             self.num_proposers,
             self.num_relays,
-            self.can_decode_threshold as f64 / self.num_relays as f64 * 100.0,
+            self.can_decode_proposal_threshold as f64 / self.num_relays as f64 * 100.0,
+            self.can_decode_block_threshold as f64 / self.num_relays as f64 * 100.0,
             self.should_decode_threshold as f64 / self.num_relays as f64 * 100.0,
             self.attestations_threshold as f64 / self.num_relays as f64 * 100.0
         );
@@ -271,11 +224,6 @@ impl MaxcpParameters {
             self.temporary_liveness_failure_probability(adv_strength)
                 .log10()
         );
-        info!(
-            "permanent liveness failure: {:.2}",
-            self.permanent_liveness_failure_probability(adv_strength)
-                .log10()
-        );
     }
 }
 
@@ -284,12 +232,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_mcp_parameters() {
-        let params = MaxcpParameters::new(2, 5);
+    fn test_maxcp_params() {
+        let params = MaxcpParameters::new(2, 100, 50);
         assert_eq!(params.num_proposers, 2);
-        assert_eq!(params.num_relays, 5);
-        assert_eq!(params.can_decode_threshold, 2);
-        assert_eq!(params.should_decode_threshold, 3);
-        assert_eq!(params.attestations_threshold, 4);
+        assert_eq!(params.num_attestors, 100);
+        assert_eq!(params.num_relays, 50);
+        assert_eq!(params.can_decode_proposal_threshold, 25);
+        assert_eq!(params.can_decode_block_threshold, 25);
+        assert_eq!(params.should_decode_threshold, 25);
+        assert_eq!(params.attestations_threshold, 50);
+    }
+
+    #[test]
+    fn test_maxcp_quick_release_params() {
+        let params = MaxcpParameters::new_quick_release(2, 100, 50);
+        assert_eq!(params.num_proposers, 2);
+        assert_eq!(params.num_attestors, 100);
+        assert_eq!(params.num_relays, 50);
+        assert_eq!(params.can_decode_proposal_threshold, 20);
+        assert_eq!(params.can_decode_block_threshold, 25);
+        assert_eq!(params.should_decode_threshold, 40);
+        assert_eq!(params.attestations_threshold, 60);
     }
 }
