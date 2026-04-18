c325d4d3a1090adbf2f429c927c155e90445b7b3
Quentin Kniep <quentin@anza.xyz>
2026-04-18 13:56:22 +0200

    nit: fix `check.sh` build (#384)

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cargo build --release --all-targets --all-features \
+cargo build --all-targets --all-features \
     && cargo clippy --all-targets --all-features -- -Dwarnings \
 	&& cargo +nightly fmt --check \
 	&& cargo doc --no-deps --document-private-items \
