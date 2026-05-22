56844bd0580defe1e4bdd67a316bf6fd477f0bb0
Quentin Kniep <hello@quentinkniep.com>
2026-05-22 20:19:43 +0200

    nit: clamp in haversine

--- src/network/simulated/ping_data.rs
+++ src/network/simulated/ping_data.rs
@@ -132,6 +132,7 @@ fn haversine_distance(lat1: f64, lon1: f64, lat2: f64, lon2: f64) -> f64 {
     let delta_lon = (lon2 - lon1).to_radians();
     let a = (delta_lat / 2.0).sin().powi(2)
         + lat1.to_radians().cos() * lat2.to_radians().cos() * (delta_lon / 2.0).sin().powi(2);
+    let a = a.min(1.0);
     let c = 2.0 * a.sqrt().asin();
     MEAN_EARTH_RADIUS * c
 }
