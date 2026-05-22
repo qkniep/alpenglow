e9d3214ec402221d44116ac8d07fecdcf82983f6
Quentin Kniep <hello@quentinkniep.com>
2026-05-22 19:06:41 +0200

    `rustfmt`

--- src/network/simulated/ping_data.rs
+++ src/network/simulated/ping_data.rs
@@ -140,9 +140,7 @@ fn haversine_distance(lat1: f64, lon1: f64, lat2: f64, lon2: f64) -> f64 {
 pub fn find_closest_ping_server(lat: f64, lon: f64) -> &'static PingServer {
     PING_SERVERS
         .iter()
-        .min_by_key(|server| {
-            haversine_distance(server.latitude, server.longitude, lat, lon) as u64
-        })
+        .min_by_key(|server| haversine_distance(server.latitude, server.longitude, lat, lon) as u64)
         .unwrap()
 }
 
