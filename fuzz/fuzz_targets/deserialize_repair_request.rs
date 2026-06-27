ebc0e5977c0c98077858b91564538ad63e3b3d4d
Quentin Kniep <hello@quentinkniep.com>
2026-06-27 13:43:47 +0200

    feat: add an MTU-sized network message deserialization helper
    Change-Id: I6494254219fc4cb8f3ac8ef3ae71ace610ea8386

--- fuzz/fuzz_targets/deserialize_repair_request.rs
+++ fuzz/fuzz_targets/deserialize_repair_request.rs
@@ -4,5 +4,5 @@ use alpenglow::repair::RepairRequest;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    let _res = wincode::deserialize::<RepairRequest>(data);
+    let _res = alpenglow::network::deserialize::<RepairRequest>(data);
 });
--- fuzz/fuzz_targets/deserialize_shred.rs
+++ fuzz/fuzz_targets/deserialize_shred.rs
@@ -4,5 +4,5 @@ use alpenglow::shredder::Shred;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    let _res = wincode::deserialize::<Shred>(data);
+    let _res = alpenglow::network::deserialize::<Shred>(data);
 });
